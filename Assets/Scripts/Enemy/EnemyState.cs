87e8d7a5dcd5ff41d5e1f1150182c8121c408a93
BlurFeng <pop297197228@gmail.com>
2026-04-08 23:55:14 +0900

    【Enemy】 创建敌人骷髅角色。

---
+++ Assets/Scripts/Enemy/EnemyState.cs
@@ -0,0 +1,12 @@
+﻿public abstract class EnemyState : EntityState
+{
+    protected Enemy Enemy { get; private set; }
+    
+    public EnemyState(Enemy enemy, StateMachine stateMachine, string stateName, string animBoolName) 
+        : base(stateMachine, stateName, animBoolName)
+    {
+        Enemy = enemy;
+        Animator = Enemy.Animator;
+        Rb = Enemy.Rb;
+    }
+}
