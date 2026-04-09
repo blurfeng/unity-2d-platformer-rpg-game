05eb89181f88dfd22e275b0e5fb8f2c0d342f23d
BlurFeng <pop297197228@gmail.com>
2026-04-09 22:01:17 +0900

    【Combat】 战斗组件，玩家和敌人添加战斗组件功能。

--- Assets/Scripts/EntityAnimationTriggers.cs
+++ Assets/Scripts/EntityAnimationTriggers.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 public class EntityAnimationTriggers : MonoBehaviour
 {
     private Entity _entity;
+    private EntityCombat _entityCombat;
 
     private void Awake()
     {
         _entity = GetComponentInParent<Entity>();
+        _entityCombat = GetComponentInParent<EntityCombat>();
     }
 
     public void CurrentStateTrigger()
     {
         _entity.CurrentStateAnimationTrigger();
+    }
+    
+    public void AttackTrigger()
+    {
+        _entityCombat.PerformAttack();
     }
 }
---
+++ Assets/Scripts/EntityCombat.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+public class EntityCombat : MonoBehaviour
+{
+    [Header("Target detection")]
+    [SerializeField] private Transform targetCheck;
+    [SerializeField] private float targetCHeckRadius = 1f;
+    [SerializeField] private LayerMask targetLayer;
+    
+    public void PerformAttack()
+    {
+        foreach (Collider2D co in GetDetectedColliders())
+        {
+            Debug.Log("Attacked " + co.name);
+        }
+    }
+
+    private Collider2D[] GetDetectedColliders()
+    {
+        return Physics2D.OverlapCircleAll(targetCheck.position, targetCHeckRadius, targetLayer);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (targetCheck) Gizmos.DrawWireSphere(targetCheck.position, targetCHeckRadius);
+    }
+}
